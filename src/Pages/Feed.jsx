7c5caf27529ed483639022b901eee2ff7b9dafea
Mohammed <mtkhanalig@gmail.com>
2023-07-09 16:36:37 +0530

    databases changes

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 import Thread from '../Component/Thread'
 
+import { database,DEV_DB_ID, COLLECTION_ID_THREADS } from '../appwriteConfig'
+
 function Feed() {
+    const [threads, setThreads] = useState()
+
+    useEffect(()=>{
+        getThreads()
+    },[])
+
+    const getThreads = async () =>{
+
+        const response = await database.listDocuments(DEV_DB_ID,)
+
+    }
+
   return (
     <div className='container mx-auto max-w-[600px]'>
         <Thread/>
--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -2,13 +2,14 @@ import { Client, Databases } from 'appwrite';
 
 const client = new Client();
 
-const VITE_ID = import.meta.env.VITE_PROJECT_ID
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
 const VITE_ENDPOINT = import.meta.env.VITE_ENDPOINT
-const DB_ID = import.meta.env.DB_ID
+const DEV_DB_ID = import.meta.env.VITE_DB_ID
+const COLLECTION_ID_THREADS = import.meta.env.VITE_COLLECTION_ID_THREADS
 
 client
     .setEndpoint(VITE_ENDPOINT)
-    .setProject(VITE_ID);
+    .setProject(PROJECT__ID);
 
 export const database = new Databases(client)
 
