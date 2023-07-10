8bac0888049b12daa8d2c809b66da1b02fe53e32
Mohammed <mtkhanalig@gmail.com>
2023-07-10 20:08:09 +0530

    commit

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -2,9 +2,12 @@ import React,{useState,useEffect} from 'react'
 import Thread from '../Component/Thread'
 import { database,DEV_DB_ID, COLLECTION_ID_THREADS } from '../appwriteConfig'
 import { Query } from 'appwrite'
+import { Image, MoreHorizontal } from 'react-feather'
 
 function Feed() {
     const [threads, setThreads] = useState([])
+
+    const [threadbody,setThreadbody] = useState('') 
 
     useEffect(()=>{
         getThreads()
@@ -26,6 +29,28 @@ function Feed() {
 
   return (
     <div className='container mx-auto max-w-[600px]'>
+
+        <div className=" p-4 ">
+            <form action="">
+                <textarea
+                
+                required
+                name='body'
+                placeholder='Say Something...'
+                value={threadbody}
+                onChange={(e)=>{setThreadbody(e.target.value)}}
+                className=' rounded-lg p-4 w-full bg-[rgba(28,29,29,1)]'
+                >
+                </textarea>
+                <div className='flex justify-between items-center'>
+                    <Image size={24}/>
+                    <input className='bg-white text-black text-sm font-bold py-2 px-4 rounded border border-gray-300 shadow' type='submit' value='Post'/>
+                </div>
+
+                
+            </form>
+        </div>
+
         {
             threads.map(thread =>(
                 <Thread key={thread.$id} thread={thread}/>
