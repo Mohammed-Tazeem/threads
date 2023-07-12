1bb3192647819c9e72bade513be415b61b0c5b0b
Mohammed <mtkhanalig@gmail.com>
2023-07-12 20:31:37 +0530

    Login Form Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Feed from './Pages/Feed';
 import { AuthProvider } from './context/AuthContext'
+import Login from './Pages/Login';
 function App() {
   return (
 
@@ -8,6 +9,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path='/' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </AuthProvider>
     </Router>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
+    const loginForm  = useRef(null)
+
   return (
-    <div>Login</div>
+    
+          <div className='container mx-auto max-w-[400px] border border-[rgba(49,49,50,1)] p-4'>
+            <form ref={loginForm} className='space-y-4 md:space-y-6'>
+                <div className="py-2">
+                    <label className='block mb-2 text-sm font-medium text-white'>Email</label>
+                    <input 
+                    type="email" 
+                    name='email'
+                    className='w-full p-2 rounded-lg'
+                    />
+                </div>
+                <div className="py-2">
+                    <label>Password</label>
+                    <input 
+                    type="password" 
+                    name='password'
+                    required
+                    className='w-full p-2 rounded-lg'
+                    />
+                </div>
+                <div className="py-2">
+                    
+                    <input 
+                    type="submit" 
+                    name='Login'
+                    className='bg-white text-black text-sm font-bold py-2 px-4 rounded border border-gray-300 shadow cursor-pointer'
+                    />
+                </div>
+            </form>
+            <p >Don't have account, Go to <Link to='/register' className='text-purple-600'>Register</Link></p>
+          </div>
+      
   )
 }
 
