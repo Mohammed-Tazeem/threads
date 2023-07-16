ca47f421cf5175ca00577ed48d6b5d11f1cddfdf
Mohammed <mtkhanalig@gmail.com>
2023-07-16 22:34:04 +0530

    Completed Till Here

--- src/Pages/ThreadPage.jsx
+++ src/Pages/ThreadPage.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import Thread from '../Component/Thread'
 import { useParams } from 'react-router-dom'
 import { database} from '../appwriteConfig'
+import { ID } from 'appwrite'
+import { useAuth } from '../context/AuthContext'
 
 function ThreadPage() {
     const {id}  = useParams()
     const [thread, setThread] = useState(null)
     const [loading, setLoading] = useState(true)
+
+    const [commentBody,setCommentBody] = useState('')
+
+    const {user} = useAuth()
 
     useEffect(()=>{
         getThread()
@@ -20,7 +26,33 @@ function ThreadPage() {
              id)
         setThread(response)
         setLoading(false)
+    }
+    const handleThreadSubmit = async (e) => {
+        e.preventDefault()
 
+       
+    }
+
+    const handleCommentSubmit =async(e) =>{
+        e.preventDefault()
+
+        const payload  = {
+            "owner_id":user.$id,
+            "thread_id":id,
+            "body":commentBody,
+        }
+
+        const response  = await database.createDocument(
+            '64aa8ecc6a139c22920c',
+            '64b400ce55e2c5ff8c9e',
+            ID.unique(),
+            payload
+        )
+
+        //console.log('RESPONSE',response);
+        //setThreads(prevState => [response, ...prevState] )
+        setCommentBody('')
+        
     }
 
     if(!loading) return
@@ -28,6 +60,27 @@ function ThreadPage() {
   return (
     <div className='container mx-auto max-w-[600px]'>
         <Thread thread={thread}/>
+        <div className=" p-4">
+            <form onSubmit={handleCommentSubmit}>
+                <textarea
+                
+                required
+                name='body'
+                placeholder='Say Something...'
+                value={commentBody}
+                onChange={(e)=>{setCommentBody(e.target.value)}}
+                className=' rounded-lg p-4 w-full bg-[rgba(28,29,29,1)]'
+                >
+                </textarea>
+                
+                
+                <div className='flex justify-end items-center border-y border-[rgba(49,49,50,1)] py-2 '>
+                    <input className='bg-white text-black text-sm font-bold py-2 px-4 rounded border border-gray-300 shadow cursor-pointer' type='submit' value='Post'/>
+                </div>
+
+                
+            </form>
+        </div>
     </div>
   )
 }
